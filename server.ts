48556e77d7e16f8d196b9de2615600abb756f96e
Nick.Haer <nhaer123@gmail.com>
2023-06-12 14:25:10 -0500

    ????

--- server.ts
+++ server.ts
@@ -10,6 +10,12 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
+try {
+    throw 'bang'
+} catch (err) {
+    console.log(err, 'rrr')
+}
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
